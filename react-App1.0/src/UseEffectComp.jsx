c824caf8e7de2d4d4d53506f9926a1914588e395
M.Suleman <muhammadsulemanm431@gmail.com>
2025-02-02 21:54:51 +0500

    conditinal run of useEffect Hook

--- react-App1.0/src/UseEffectComp.jsx
+++ react-App1.0/src/UseEffectComp.jsx
@@ -5,14 +5,38 @@ import { useState, useEffect } from "react"
 function UseEffectComp() {
 
     let [counter, setCounter]  = useState(0);
+    let [name, setName]  = useState("Suleman");
+
+    // useEffect(() =>{
+    //     console.log("Hello iam useEffect.. " + counter)
+    // } )
+
+    // useEffect(() =>{
+    //     console.log("Hello iam useEffect.. " + counter)
+    // }, [] )
+
+    // useEffect(() =>{
+    //     console.log("Hello iam useEffect.. " + counter)
+    // }, [counter] )
+
+    // useEffect(() =>{
+    //     console.log("Hello iam useEffect.. " + counter)
+    // }, [counter, name] )
+
     useEffect(() =>{
         console.log("Hello iam useEffect.. " + counter)
-    } )
+    }, [counter] )
+
+    useEffect(() =>{
+        console.log("Hello iam useEffect.. " + name)
+    }, [name] )
 
     return (
         <div>
             <h1>{counter}</h1>
+            <h1>{name}</h1>
             <button type="button" onClick={() => {setCounter(counter + 1)}} >Update State</button>
+            <button type="button" onClick={() => {setName("Muzamil")}} >Update Name</button>
         </div>
     )
 }
