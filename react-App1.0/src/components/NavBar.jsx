f1d0bbcd778fe35899bdc90f44f17369a642843b
M.Suleman <muhammadsulemanm431@gmail.com>
2025-02-06 23:52:32 +0500

    58 | Active Link Using React Router In React App | React NavLink

--- react-App1.0/src/components/NavBar.jsx
+++ react-App1.0/src/components/NavBar.jsx
@@ -1,12 +1,23 @@
-import { Link } from "react-router-dom"
-
+// import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
+
+    const NavLinkFun = ({isActive}) => {
+        return { 
+        fontWeight: isActive ? 'bold' : 'normal',
+        fontSize: isActive ? '23px' : '20px'
+    }
+    }
     return (
         <nav>
-            <Link  to='/' >Home</Link>
+            {/* <Link  to='/' >Home</Link>
             <Link  to='/about' >About</Link>
-            <Link  to='/contact' >Contact</Link>
+            <Link  to='/contact' >Contact</Link> */}
+
+            <NavLink style={NavLinkFun}  to='/' >Home</NavLink>
+            <NavLink style={NavLinkFun} to='/about' >About</NavLink>
+            <NavLink style={NavLinkFun} to='/contact' >Contact</NavLink>
         </nav>
     )
 }
